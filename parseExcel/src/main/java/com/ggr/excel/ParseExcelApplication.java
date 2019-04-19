434072c54b7b73e9a54002de12c14b789dd0e79a
1107719463@qq.com <guirong489309>
2019-04-19 19:39:18 +0800

    添加新项目

---
+++ parseExcel/src/main/java/com/ggr/excel/ParseExcelApplication.java
@@ -0,0 +1,13 @@
+package com.ggr.excel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParseExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParseExcelApplication.class, args);
+	}
+
+}

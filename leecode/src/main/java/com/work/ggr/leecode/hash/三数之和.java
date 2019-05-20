b41145899151f1358dcb1946532ee7bdd60447d8
真的轨迹 <1107719463@qq.com>
2019-05-20 16:04:44 +0800

    leecode

--- leecode/src/main/java/com/work/ggr/leecode/hash/三数之和.java
+++ leecode/src/main/java/com/work/ggr/leecode/hash/三数之和.java
@@ -12,6 +12,7 @@ import java.util.Set;
  * 满足要求的三元组集合为： [ [-1, 0, 1], [-1, -1, 2] ]
  * 
  * @author gr
+ *更改为双指针的方式获取结果之和
  *
  */
 
@@ -45,7 +46,24 @@ public class 三数之和 {
 		Arrays.sort(nums);
 		for(int i=0;i<nums.length-2;i++) {
 			if(i==0||(i>0&&nums[i]!=nums[i-1])) {
-				
+				int index1=i+1;
+				int index2=nums.length-1;
+				while (index1<index2) {
+					if(nums[i]+nums[index1]+nums[index2]==0) {
+						result.add(Arrays.asList(nums[i],nums[index1],nums[index2]));
+						while(index1<index2&&nums[index1]==nums[index1+1]) {
+							index1++;
+						}
+						while(index2>index1&&nums[index2]==nums[index2-1]) {
+							index2--;
+						}
+						index1++;index2--;
+					}else if(nums[i]+nums[index1]+nums[index2]<0) {
+						index1++;
+					}else {
+						index2--;
+					}
+				}
 			}
 		}
 		return result;

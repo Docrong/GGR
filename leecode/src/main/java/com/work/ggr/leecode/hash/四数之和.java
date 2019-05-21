222f74fcb8072bcd69733b9ff6b7c4bc371a97cc
真的轨迹 <1107719463@qq.com>
2019-05-21 16:34:10 +0800

    leetcode

--- leecode/src/main/java/com/work/ggr/leecode/hash/四数之和.java
+++ leecode/src/main/java/com/work/ggr/leecode/hash/四数之和.java
@@ -23,41 +23,78 @@ public class 四数之和 {
 
 	public static void main(String[] args) {
 
-		
-		int[]nums= {1,0,-1,0,-2,2};
-		int target=0;
-		List result=fourSum(nums, target);
+		int[] nums = { 1, 0, -1, 0, -2, 2 };
+		int target = 0;
+		List result = fourSum(nums, target);
 		System.out.println(result);
 	}
 
 	public static List fourSum(int[] nums, int target) {
-		List result=new ArrayList();
+		/*List result = new ArrayList();
 		Arrays.sort(nums);
-		for(int i=0;i<nums.length-3;i++) {
-			for(int index1=i+1;index1<nums.length-2;index1++) {
-				int index2=index1+1;
-				int index3=nums.length-1;
-				
-				while(index2<index3) {
-				if(nums[i]+nums[index1]+nums[index2]+nums[index3]==target) {
-					result.add(Arrays.asList(nums[i],nums[index1],nums[index2],nums[index3]));
-					while(index2<index3&&nums[index2]==nums[index2+1]) {
-						index2++;index3--;
+		for (int i = 0; i < nums.length - 3; i++) {
+			for (int index1 = i + 1; index1 < nums.length - 2; index1++) {
+				int index2 = index1 + 1;
+				int index3 = nums.length - 1;
+
+				while (index2 < index3) {
+					if (nums[i] + nums[index1] + nums[index2] + nums[index3] == target) {
+						result.add(Arrays.asList(nums[i], nums[index1], nums[index2], nums[index3]));
+						while (index2 < index3 && nums[index2] == nums[index2 + 1]) {
+							index2++;
+							index3--;
+						}
+						while (index2 < index3 && nums[index3] == nums[index3 - 1]) {
+							index2++;
+							index3--;
+						}
+						index2++;
+						index3--;
+					} else if (nums[i] + nums[index1] + nums[index2] + nums[index3] < target) {
+						index2++;
+					} else {
+						index3--;
 					}
-					while(index2<index3&&nums[index3]==nums[index3-1]) {
-						index2++;index3--;
-					}
-					index2++;index3--;
-				}else if(nums[i]+nums[index1]+nums[index2]+nums[index3]<target) {
-					index2++;
-				}else {
-					index3--;
-				}
 				}
 			}
-			
+
 		}
 		return result;
-
+*/
+		 Arrays.sort(nums);
+	        List<List<Integer>> ls = new ArrayList<>();
+	        for (int i = 0; i < nums.length - 3; i++) {
+	            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) { 
+	                for (int j = i + 1; j < nums.length - 2; j++) {
+	                    if (j == i + 1 || (j > i + 1 && nums[j] != nums[j - 1])) {
+	                        int l = j + 1, r = nums.length - 1, sum = target - nums[j] - nums[i];
+	                        while (l < r) {
+	                            if (nums[l] + nums[r] == sum) {
+	                                ls.add(Arrays.asList(nums[i], nums[j], nums[l], nums[r]));
+	                                while (l < r && nums[l] == nums[l + 1]) {
+	                                    l++;
+	                                }
+	                                while (l < r && nums[r] == nums[r - 1]) {
+	                                    r--;
+	                                }
+	                                l++;
+	                                r--;
+	                            } else if (nums[l] + nums[r] < sum) {
+	                                while (l < r && nums[l] == nums[l + 1]) {
+	                                    l++;
+	                                } 
+	                                l++;
+	                            } else {
+	                                while (l < r && nums[r] == nums[r - 1]) {
+	                                    r--;
+	                                }
+	                                r--;
+	                            }
+	                        }
+	                    }
+	                }
+	            }
+	        }
+	        return ls;
 	}
 }

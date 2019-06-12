f22529bd07427e1fa374f31c514da7de40bc9119
真的轨迹 <1107719463@qq.com>
2019-06-12 15:36:37 +0800

    提交

--- webservice/src/main/java/com/ggr/client/HelloServiceClient.java
+++ webservice/src/main/java/com/ggr/client/HelloServiceClient.java
@@ -18,8 +18,8 @@ public class HelloServiceClient {
 				//设置地址
 				call.setTargetEndpointAddress("http://localhost:8080/webservice/services/HelloServiceImpl?wsdl");
 				//设置要执行的方法(以下两种方式都可以)
-//				call.setOperationName("sayHello");
-				call.setOperationName(new QName("http://impl.service.ggr.com","sayHello"));
+				call.setOperationName("sayHello");
+//				call.setOperationName(new QName("http://impl.service.ggr.com","sayHello"));
 				//设置要传入参数,如果没有要传入的参数，则不要写这个（参数名、参数类型、ParameterMode）,第一个参数啊没有具体要求
 				call.addParameter("param1", org.apache.axis.Constants.XSD_STRING,javax.xml.rpc.ParameterMode.IN);
 				//设置返回的类型

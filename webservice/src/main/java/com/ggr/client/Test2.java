15bf5394c5d6b98fa467f672760d41a6caac501d
真的轨迹 <1107719463@qq.com>
2019-06-18 14:13:18 +0800

    tijiao

---
+++ webservice/src/main/java/com/ggr/client/Test2.java
@@ -0,0 +1,15 @@
+package com.ggr.client;
+
+public class Test2 {
+
+	
+	public static void main(String[] args) {
+		double []price= {529,164.5,334.5+30};
+		double []price2= {506,164.5,311.5};
+		//所以鞋垫的23不影响最后结果
+		float bill=(float) ((529-60)*0.75);
+		System.out.println(bill);
+		
+		System.out.println(311.5-247);
+	}
+}

75242ce40a5cfe61e6e0c54140d75477a31ffd77
真的轨迹 <1107719463@qq.com>
2019-08-22 17:46:04 +0800

    添加配置
    Signed-off-by: 真的轨迹 <1107719463@qq.com>

--- blog/src/main/java/com/ggr/blog/dao/impl/CommentsDaoHibernate.java
+++ blog/src/main/java/com/ggr/blog/dao/impl/CommentsDaoHibernate.java
@@ -4,13 +4,18 @@ import com.ggr.blog.dao.CommentsDao;
 import com.ggr.blog.model.Comments;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.HibernateCallback;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Repository("iCommentsDao")
 public class CommentsDaoHibernate  extends HibernateDaoSupport implements CommentsDao {
@@ -18,7 +23,7 @@ public class CommentsDaoHibernate  extends HibernateDaoSupport implements Commen
 
     protected Log log = LogFactory.getLog(getClass());
 
-    private SessionFactory sessionFactory;
+
 
 
     @Override
@@ -26,7 +31,16 @@ public class CommentsDaoHibernate  extends HibernateDaoSupport implements Commen
         log.info(this.getClass().getName());
         System.out.println("sessionFactory321");
 
+        HibernateCallback callback = new HibernateCallback(){
 
+            @Override
+            public Object doInHibernate(Session session) throws HibernateException {
+                Query query = session.createSQLQuery("select sysdate from dual");
+                List list=query.list();
+                System.out.println(list);
+                return null;
+            }
+        };
 
 
         return null;

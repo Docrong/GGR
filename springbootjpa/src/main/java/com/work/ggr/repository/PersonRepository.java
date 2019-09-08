877bfede14c084c2c58b9051052db5a149765d63
真的轨迹 <1107719463@qq.com>
2019-09-08 14:44:00 +0800

    1

---
+++ springbootjpa/src/main/java/com/work/ggr/repository/PersonRepository.java
@@ -0,0 +1,19 @@
+package com.work.ggr.repository;
+
+import com.work.ggr.model.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * @author : gr
+ * @date : 2019/9/6 16:18
+ */
+@Repository("personRepository")
+public interface PersonRepository extends JpaRepository<Person,Integer> {
+
+    Person findById(String id);
+    List<Person> findByUsernameAndAddress(String username, String address);
+}
---
+++ springbootjpa/src/main/java/com/work/ggr/service/PersonServiceImpl2.java
@@ -0,0 +1,17 @@
+package com.work.ggr.service;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+
+/**
+ * @author : gr
+ * @date : 2019/9/8 13:48
+ */
+@Transactional(value = "transactionManagerSecond")
+public class PersonServiceImpl2 {
+
+    public void testSecondDS(Map maptj){
+
+    }
+}

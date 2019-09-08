48d2349a1795d9c2e00cc2d569256a2a31f7d9aa
郭贵荣 <guoguirong@boco.com.cn>
2019-09-08 20:39:15 +0800

    提交更改

--- springbootjpa/src/main/java/com/work/ggr/ds2/repository2/PersonRepository2.java
+++ springbootjpa/src/main/java/com/work/ggr/ds2/repository2/PersonRepository2.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository("personRepository2")
-public interface PersonRepository extends JpaRepository<Person,Integer> {
+public interface PersonRepository2 extends JpaRepository<Person,Integer> {
 }
--- springbootjpa/src/main/java/com/work/ggr/ds2/service2/PersonServiceImpl2.java
+++ springbootjpa/src/main/java/com/work/ggr/ds2/service2/PersonServiceImpl2.java
@@ -1,7 +1,7 @@
 package com.work.ggr.ds2.service2;
 
 import com.work.ggr.model.Person;
-import com.work.ggr.ds2.repository2.PersonRepository;
+import com.work.ggr.ds2.repository2.PersonRepository2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -21,7 +21,7 @@ public class PersonServiceImpl2 {
 
     @Autowired
     @Qualifier(value = "personRepository2")
-    private PersonRepository personRepository;
+    private PersonRepository2 personRepository;
     @Transactional(value = "transactionManagerSecond")
     public void testSecondDS(Map maptj){
         Person p2=new Person();
